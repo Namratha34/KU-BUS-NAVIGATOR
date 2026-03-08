a4c27e89df534a135c689145226b6b0583478393
Namratha <namrathaachar7204@gmail.com>
2026-03-08 22:38:04 +0530

    Add init file and update db connection for TiDB

--- backend/db.py
+++ backend/db.py
@@ -3,9 +3,12 @@ import os
 
 def get_db_connection():
     return mysql.connector.connect(
-        host=os.getenv("mysql.railway.internal"),
-        user=os.getenv("root"),
-        password=os.getenv("QlUHxrpygbWZwbnAwcWmlsSfdTMixSmU"),
-        database=os.getenv("railway"),
-        port=int(os.getenv("3306"))
+        host=os.environ.get('HOST'),
+        user=os.environ.get('USER'),
+        password=os.environ.get('PASSWORD'),
+        database=os.environ.get('NAME'),
+        port=int(os.environ.get('PORT', 4000)),
+        # Use these two lines instead of a CA Path:
+        ssl_verify_identity=True, 
+        ssl_disabled=False
     )
